e93c5dec87052443a297c6681ad507728b0083f5
Dejan Mitrovic <dejanmitrovic1910@proton.me>
2026-03-23 13:16:40 +0100

    chore: remove home and additional page

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,9 +18,9 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">Home</s-link>
+        {/* Future nav items: <s-link href="/app">Home</s-link> */}
         <s-link href="/app/tickets/import">Import ticket codes</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
+        {/* <s-link href="/app/additional">Additional page</s-link> */}
       </s-app-nav>
       <Outlet />
     </AppProvider>
